f8db2921a1aabfc086ceaf73a1e05670c5603549
mshakya <microbeatic@gmail.com>
2016-11-19 00:29:31 -0700

    travis yml and printing what it ran

--- test_data/test_whole_pipeline.sh
+++ test_data/test_whole_pipeline.sh
@@ -7,13 +7,21 @@ printf "gr1\ngr2\ngr1\n" > test_gr.txt
 paste test_id.txt testR1.txt testR2.txt test_gr.txt > test_ed.txt
 rm test_id.txt testR1.txt testR2.txt test_gr.txt
 sed -i 's/:\t/:/g' test_ed.txt
-echo -e "ID\tRawreads_files\tgroup" | cat - test_ed.txt > test_experimental_design.txt 
+echo -e "ID\tRawreads_files\tgroup" | cat - test_ed.txt > test_experimental_design.txt
 rm test_ed.txt
 
 
 #
 printf "running pipeline now"
-rm -r pipeline_test
+
+printf "perl ../scripts/runPipeline_rRNA_noqsub_commandline.pl -test_kingdom prokaryote \
+-significant_pvalue 0.001 -exp test_experimental_design.txt \
+-d pipeline_test \
+-prokaryote_fasta data/test_prok.fa \
+-eukarya_fasta data/test_euk.fa -index_ref_bt2 test_index \
+-gff_prokaryote data/test_prok.gff \
+-test_method EdgeR -gene_coverage_fasta data/test_prok.fa"
+
 perl ../scripts/runPipeline_rRNA_noqsub_commandline.pl -test_kingdom prokaryote \
 -significant_pvalue 0.001 -exp test_experimental_design.txt \
 -d pipeline_test \
