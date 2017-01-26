9ad2e2a17d13d8869812f99c47dad7db3edde47c
Migun Shakya <microbeatic@gmail.com>
2017-01-25 17:32:06 -0700

    fixed a small issue

--- test_data/test_pipeline_MacOSX.sh
+++ test_data/test_pipeline_MacOSX.sh
@@ -7,14 +7,14 @@ printf "gr1\ngr2\ngr1\ngr1\ngr2\ngr2\n" > test_gr.txt
 paste -d "\0" testR1.txt testR2.txt > testR1R2.txt
 paste test_id.txt testR1R2.txt test_gr.txt > test_ed.txt
 rm test_id.txt testR1.txt testR2.txt testR1R2.txt test_gr.txt
-echo -e "ID\tRawreads_files\tgroup" | cat - test_ed.txt > experimental_design.txt
+echo "ID\tRawreads_files\tgroup" | cat - test_ed.txt > test_experimental_design.txt
 rm test_ed.txt
 
 
 
 printf "running pipeline now"
 
-printf "perl ../scripts/runPipeline_rRNA_noqsub_commandline.pl -test_kingdom both \
+printf "perl ../runPipeline_rRNA_noqsub_commandline.pl -test_kingdom both \
 -significant_pvalue 0.001 -exp test_experimental_design.txt \
 -d pipeline_test_both \
 -prokaryote_fasta data/test_prok.fa \
@@ -22,8 +22,8 @@ printf "perl ../scripts/runPipeline_rRNA_noqsub_commandline.pl -test_kingdom bot
 -gff_eukarya data/eukarya_test.gff3 -gff_prokaryote data/test_prok.gff \
 -test_method both -gene_coverage_fasta data/test_prok.fa"
 
-perl ../scripts/runPipeline_rRNA_noqsub_commandline.pl -test_kingdom both \
--significant_pvalue 0.001 -exp experimental_design.txt \
+perl ../runPipeline_rRNA_noqsub_commandline.pl -test_kingdom both \
+-significant_pvalue 0.001 -exp test_experimental_design.txt \
 -d pipeline_test_both \
 -prokaryote_fasta data/test_prok.fa \
 -eukarya_fasta data/eukarya_test.fa -index_ref_bt2 test_index \
