07f2d672a15acf583b3d52eddbccaec79a4731ea
Migun Shakya <microbeatic@gmail.com>
2017-02-06 13:13:07 -0700

    added the sort command to have forward and reverse

--- test_data/test_pipeline_linux.sh
+++ test_data/test_pipeline_linux.sh
@@ -2,8 +2,8 @@
 
 # create a test experimental design file
 
-find fastqs -iname "*test*R2.fastq" -exec readlink -f {} \; > testR2.txt
-find fastqs -iname "*test*R1.fastq" -exec readlink -f {} \; | sed 's/R1\.fastq/R1\.fastq:/g' > testR1.txt
+find fastqs -iname "*test*R2.fastq" -exec readlink -f {} \; | sort > testR2.txt
+find fastqs -iname "*test*R1.fastq" -exec readlink -f {} \; | sort | sed 's/R1\.fastq/R1\.fastq:/g' > testR1.txt
 printf "samp1\nsamp2\nsamp3\nsamp4\nsamp5\nsamp6\n" > test_id.txt
 printf "liver\nspleen\nspleen\nliver\nliver\nspleen\n" > test_gr.txt
 paste test_id.txt testR1.txt testR2.txt test_gr.txt > test_ed.txt
