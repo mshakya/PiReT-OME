d3a4cf17d0cc44f336703e240e175849c6ba12e3
Migun Shakya <microbeatic@gmail.com>
2017-02-16 10:48:32 -0700

    removed unnecessary lines

--- scripts/readmapping.sh
+++ scripts/readmapping.sh
@@ -1,15 +1,4 @@
 #!/bin/sh
-##$ -V
-## -N trim_readmapping_$sample
-##$ -l h_vmem=10G
-##$ -pe smp 10
-##$ -m abe
-##$ -M sfeng@lanl.gov
-##$ -j y
-##$ -o /users/203270/scratch/trim_readmapping.log
-
-
-
 
 date
 #map to ref
@@ -18,9 +7,7 @@ perl $scriptDir/rRNA_reads_mapping.pl -test $test -cpu $numCPU  -p1  $workdir/$s
 
 ###classification
 
-date
 #echo "perl $scriptDir/../../scripts/bwa_sam2classification.pl -p1 unmapped.$sample.1.fastq -p2 unmapped.$sample.2.fastq -u unmapped.$sample.unpaired.fastq -cpu $numCPU"
 #cd $workdir/$sample/mapping_results/
 #perl $scriptDir/../../scripts/bwa_sam2classification.pl -p1 unmapped.$sample.1.fastq -p2 unmapped.$sample.2.fastq -u unmapped.$sample.unpaired.fastq -cpu $numCPU  
 
-date
