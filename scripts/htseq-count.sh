5554d7123b18ffcf54c00b26f2d0ebf31b4dd69b
Migun Shakya <microbeatic@gmail.com>
2017-02-23 17:04:09 -0700

    this script isobsolete, should be removed eventually

--- scripts/htseq-count.sh
+++ scripts/htseq-count.sh
@@ -1,7 +1,9 @@
-#!/code/bin/sh
-
-echo"$scriptDir/htseq-count.pl $workdir $sample $test\n"
-
-$scriptDir/htseq-count.pl $workdir $sample $test
+#!/bin/sh
 
 
+##$PBS_O_WORKDIR=$workdir
+##$PBS_O_PATH=$path
+
+#cd $PBS_O_WORKDIR
+echo "perl $scriptDir/htseq-count.pl $workdir $sample $test\n"
+perl $scriptDir/htseq-count.pl $workdir $sample $test
