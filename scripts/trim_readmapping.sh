fcbdb1709fdf78fddcfaa40233616adb3d084067
Migun Shakya <microbeatic@gmail.com>
2017-03-14 17:34:33 -0600

    removed not needed commented lines

--- scripts/trim_readmapping.sh
+++ scripts/trim_readmapping.sh
@@ -1,20 +1,14 @@
 #!/bin/sh
 
 
-#plib=`cpan -D String::Approx | grep Approx.pm | sed 's/\/String\/Approx\.pm//g'`
-#echo $plib
-
-
-#import Perl path
-#export PERL5LIB="$ROOTDIR/ext/lib/perl5:$plib:$PERL5LIB"
-
 #QC step
 echo "perl $scriptDir/illumina_fastq_QC.pl  -min_L 60 -n 5 -q 15 -lc 0.7 -t $numCPU -prefix $sample -d $workdir/$sample/trimming_results/ -p  $rawreads"
 perl $scriptDir/illumina_fastq_QC.pl  -min_L 60 -n 5 -q 15 -lc 0.7 -t $numCPU -prefix $sample -d $workdir/$sample/trimming_results/ -p  $rawreads
 
 #map to reference
-echo "perl $scriptDir/rRNA_reads_mapping.pl -cpu $numCPU  -p1  $workdir/$sample/trimming_results/$sample.1.trimmed.fastq -p2 $workdir/$sample/trimming_results/$sample.2.trimmed.fastq -prefix $sample -index $indexref    -o $workdir"
+echo "perl $scriptDir/rRNA_reads_mapping.pl -cpu $numCPU -p1 $workdir/$sample/trimming_results/$sample.1.trimmed.fastq -p2 $workdir/$sample/trimming_results/$sample.2.trimmed.fastq -prefix $sample -index $indexref -o $workdir"
 perl $scriptDir/rRNA_reads_mapping.pl -cpu $numCPU -p1 $workdir/$sample/trimming_results/$sample.1.trimmed.fastq -p2 $workdir/$sample/trimming_results/$sample.2.trimmed.fastq -prefix $sample -index $indexref -o $workdir
+
 # parse BAM files
 echo "perl $scriptDir/parse_BAMfile.pl -bamfile $rawreads -sample $sample -o $workdir"
 perl $scriptDir/parse_BAMfile.pl -bamfile $rawreads -sample $sample -o $workdir
