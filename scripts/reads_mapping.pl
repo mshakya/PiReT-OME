0526f635f011a4b17ba5f2c37d96c1c3b0fcb31c
Migun Shakya <microbeatic@gmail.com>
2017-03-23 22:26:03 -0600

    added option and remmoved duplicate calling of subroutine

--- scripts/reads_mapping.pl
+++ scripts/reads_mapping.pl
@@ -8,17 +8,11 @@ use Map::Mapping;
 use Getopt::Long;
 use File::Basename;
 
-# perl $scriptDir/rRNA_reads_mapping.pl-test $test -cpu $numCPU
-#-p1  $workdir/$sample/trimming_results/$sample.1.trimmed.fastq
-#-p2 $workdir/$sample/trimming_results/$sample.2.trimmed.fastq
-#-sample $sample
-#-index $indexref
-#-o $workdir
-
-my ($pairedReadsFile1,  $indexFile, $pairedReadsFile2,
-    $unpairedReadsFile, $workdir,   $euk_ref,
-    $hisat2opts,        $sample,    $prok_ref,
-    $sampleDir,         $mapDir,    $splicesite
+my ($pairedReadsFile1,  $indexFile,     $pairedReadsFile2,
+    $unpairedReadsFile, $workdir,       $euk_ref,
+    $hisat2opts,        $sample,        $prok_ref,
+    $sampleDir,         $mapDir,        $splicesite,
+    $samindex,          $samindex_prok, $samindex_euk
 );
 my $version = "develop";
 my $kingdom = 'prokaryote';
@@ -37,6 +31,8 @@ GetOptions(
     'E|euk_ref=s'        => \$euk_ref,
     'B|prok_ref=s'       => \$prok_ref,
     'T|splicesite=s'     => \$splicesite,
+    'XP|samin_prok=s'      => \$samindex_prok,
+    'XE|samin_euk=s'       => \$samindex_euk,
     'V|version'          => sub { printVersion() },
     'help|h|?'           => sub { &Usage() }
 );
@@ -96,6 +92,9 @@ OPTIONS
 
         -T, --splicesite
             tab delimited file with information on splice sites
+
+        -X, --samindex
+            tab delimited file generated from samtools faidx
 
 END
     exit;
@@ -163,8 +162,6 @@ $mapDir = join '/', ( $sampleDir, 'mapping_results' );
 mkdir $mapDir if ( !-e $mapDir );
 if ( !-e $mapDir ) { print "cannot make dir $mapDir\n"; }
 
-# Mapping::open_samfiles( $kingdom, $mapDir );
-
 Mapping::runMapping(
     r1             => $pairedReadsFile1,
     r2             => $pairedReadsFile2,
@@ -176,107 +173,138 @@ Mapping::runMapping(
     mappingLogFile => "$mapDir/mapped.log"
 );
 
-open( UMAP1, ">", "$mapDir/unmap_1.fastq" ) or die "Damn. $!";
-open( UMAP2, ">", "$mapDir/unmap_2.fastq" ) or die "Damn. $!";
+open( my $UMAP1, ">", "$mapDir/unmap_1.fastq" ) or die "Damn. $!";
+open( my $UMAP2, ">", "$mapDir/unmap_2.fastq" ) or die "Damn. $!";
 
 if ( $kingdom eq 'both' ) {
-    open( PR_EUK, ">", "$mapDir/paired_euk.sam" ) or die "Damn. $!";
-    open( PR_PROK, ">", "$mapDir/paired_prok.sam" )
+    open( my $PR_EUK, ">", "$mapDir/paired_euk.sam" ) or die "Damn. $!";
+    open( my $PR_PROK, ">", "$mapDir/paired_prok.sam" )
         or die "Damn. $!";
-    open( NP_EUK, ">", "$mapDir/notproper_euk.sam" )
+    open( my $NP_EUK, ">", "$mapDir/notproper_euk.sam" )
         or die "Damn. $!";
-    open( NP_PROK, ">", "$mapDir/notproper_prok.sam" )
+    open( my $NP_PROK, ">", "$mapDir/notproper_prok.sam" )
         or die "Damn. $!";
-    open( FW_PROK, ">", "$mapDir/fwd_prok.sam" )
+    open( my $FW_PROK, ">", "$mapDir/fwd_prok.sam" )
         or die "Damn. $!";
-    open( RV_PROK, ">", "$mapDir/rev_prok.sam" )
+    open( my $RV_PROK, ">", "$mapDir/rev_prok.sam" )
         or die "Damn. $!";
-    open( FW_EUK, ">", "$mapDir/fwd_euk.sam" ) or die "Damn. $!";
-    open( RV_EUK, ">", "$mapDir/rev_euk.sam" ) or die "Damn. $!";
-    open( FW_EUK_FQ, ">", "$mapDir/fwd_euk_unmapped.fastq" ) or die "Damn. $!";
-    open( RV_EUK_FQ, ">", "$mapDir/rev_euk_unmapped.fastq" ) or die "Damn. $!";
+    open( my $FW_EUK, ">", "$mapDir/fwd_euk.sam" ) or die "Damn. $!";
+    open( my $RV_EUK, ">", "$mapDir/rev_euk.sam" ) or die "Damn. $!";
+    open( my $FW_EUK_FQ, ">", "$mapDir/fwd_euk_unmapped.fastq" )
+        or die "Damn. $!";
+    open( my $RV_EUK_FQ, ">", "$mapDir/rev_euk_unmapped.fastq" )
+        or die "Damn. $!";
+    open( my $FW_PROK_FQ, ">", "$mapDir/fwd_prok_unmapped.fastq" )
+        or die "Damn. $!";
+    open( my $RV_PROK_FQ, ">", "$mapDir/rev_prok_unmapped.fastq" )
+        or die "Damn. $!";
     open( FH, "$mapDir/mapped.sam" ) or die "Damn. $!";
-    
+
     while (<FH>) {
-    chomp;
-    next if (/^\@/);
-    my $samline = $_;
-    my @samFields = split /\t/, $samline;
-    Mapping::parsePairedUnmapped( $samline, UMAP1, UMAP2 );
-    if ( $samFields[2] eq )
-    
-    Mapping::parsePairedMapped(
-        samline    => $samline,
-        paired_out => $Pmap,
-        nonproper  => $Nmap
-    );
-    
-    Mapping::parseSingles()
+        chomp;
+        next if (/^\@/);
+        my $samline = $_;
+        my @samFields = split /\t/, $samline;
+
+        Mapping::parsePairedUnmapped( $samline, $UMAP1, $UMAP2 );
+        my %prok_id = Mapping::parseFAI($samindex_prok);
+        my %euk_id  = Mapping::parseFAI($samindex_euk);
+        if ( $euk_id{ $samFields[2] } ) {
+            Mapping::parsePairedMapped(
+                samline    => $samline,
+                paired_out => $PR_PROK,
+                nonproper  => $NP_PROK
+            );
+            Mapping::parseSingles(
+                samline  => $samline,
+                unMapFwd => $FW_EUK_FQ,
+                unMapRev => $RV_EUK_FQ,
+                MapFwd   => $FW_EUK,
+                MapRev   => $RV_EUK
+            );
+        }
+        elsif ( $prok_id{ $samFields[2] } ) {
+            Mapping::parsePairedMapped(
+                samline    => $samline,
+                paired_out => $PR_EUK,
+                nonproper  => $NP_EUK
+            );
+            Mapping::parseSingles(
+                samline  => $samline,
+                unMapFwd => $FW_PROK_FQ,
+                unMapRev => $RV_PROK_FQ,
+                MapFwd   => $FW_PROK,
+                MapRev   => $RV_PROK
+            );
+        }
     }
-}
-elif( $kingdom eq 'prokaryote' ) {
-    open( PR_PROK, ">", "$mapDir/paired_prok.sam" )
-        or die "Damn. $!";
-    open( NP_PROK, ">", "$mapDir/notproper_prok.sam" )
-        or die "Damn. $!";
-    open( FW_PROK, ">", "$mapDir/fwd_prok.sam" )
-        or die "Damn. $!";
-    open( RV_PROK, ">", "$mapDir/rev_prok.sam" )
-        or die "Damn. $!";
-    open( FW_PROK_FQ, ">", "$mapDir/fwd_prok_unmapped.fastq" ) or die "Damn. $!";
-    open( RV_PROK_FQ, ">", "$mapDir/rev_prok_unmapped.fastq" ) or die "Damn. $!";
-    open( FH, "$mapDir/mapped.sam" ) or die "Damn. $!";
-    
-    while (<FH>) {
-    chomp;
-    next if (/^\@/);
-    my $samline = $_;
-    Mapping::parsePairedUnmapped( $samline, UMAP1, UMAP2 );
-    Mapping::parsePairedMapped(
-        samline    => $samline,
-        paired_out => PR_PROK,
-        nonproper  => NP_PROK
-    );
-    Mapping::parseSingles(
-        samline  => $samline,
-        unMapFwd => $FR,
-        unMapRev => $RR,
-        MapFwd   => $FM,
-        MapRev   => $RM
-    );
-}
-elif( $kingdom eq 'eukarya' ) {
-    open( PR_EUK, ">", "$mapDir/paired_euk.sam" ) or die "Damn. $!";
-    open( NP_EUK, ">", "$mapDir/notproper_euk.sam" )
-        or die "Damn. $!";
-    open( FW_EUK, ">", "$mapDir/fwd_prok.sam" ) or die "Damn. $!";
-    open( RV_EUK, ">", "$mapDir/rev_euk.sam" ) or die "Damn. $!";
-    open( FW_EUK_FQ, ">", "$mapDir/fwd_euk_unmapped.fastq" ) or die "Damn. $!";
-    open( RV_EUK_FQ, ">", "$mapDir/rev_euk_unmapped.fastq" ) or die "Damn. $!";
-    Mapping::parsePairedUnmapped( $samline, UMAP1, UMAP2 );
-    Mapping::parsePairedMapped(
-        samline    => $samline,
-        paired_out => PR_EUK,
-        nonproper  => NP_EUK
+
 }
 
-open( FH, "$mapDir/mapped.sam" ) or die "Damn. $!";
-while (<FH>) {
-    chomp;
-    next if (/^\@/);
-    my $samline = $_;
-    Mapping::parsePairedUnmapped( $samline, $unmap1, $unmap2 );
-    Mapping::parsePairedMapped(
-        samline    => $samline,
-        paired_out => $Pmap,
-        nonproper  => $Nmap
-    );
+elsif ( $kingdom eq 'prokaryote' ) {
+    open( my $PR_PROK, ">", "$mapDir/paired_prok.sam" )
+        or die "Damn. $!";
+    open( my $NP_PROK, ">", "$mapDir/notproper_prok.sam" )
+        or die "Damn. $!";
+    open( my $FW_PROK, ">", "$mapDir/fwd_prok.sam" )
+        or die "Damn. $!";
+    open( my $RV_PROK, ">", "$mapDir/rev_prok.sam" )
+        or die "Damn. $!";
+    open( my $FW_PROK_FQ, ">", "$mapDir/fwd_prok_unmapped.fastq" )
+        or die "Damn. $!";
+    open( my $RV_PROK_FQ, ">", "$mapDir/rev_prok_unmapped.fastq" )
+        or die "Damn. $!";
+    open( FH, "$mapDir/mapped.sam" ) or die "Damn. $!";
+
+    while (<FH>) {
+        chomp;
+        next if (/^\@/);
+        my $samline = $_;
+        Mapping::parsePairedUnmapped( $samline, $UMAP1, $UMAP2 );
+        Mapping::parsePairedMapped(
+            samline    => $samline,
+            paired_out => $PR_PROK,
+            nonproper  => $NP_PROK
+        );
+        Mapping::parseSingles(
+            samline  => $samline,
+            unMapFwd => $FW_PROK_FQ,
+            unMapRev => $RV_PROK_FQ,
+            MapFwd   => $FW_PROK,
+            MapRev   => $RV_PROK
+        );
+    }
 }
-close $Nmap;
-close $Pmap;
-close $unmap1;
-close $unmap2;
-close FH;
+elsif ( $kingdom eq 'eukarya' ) {
+    open( my $PR_EUK, ">", "$mapDir/paired_euk.sam" ) or die "Damn. $!";
+    open( my $NP_EUK, ">", "$mapDir/notproper_euk.sam" )
+        or die "Damn. $!";
+    open( my $FW_EUK, ">", "$mapDir/fwd_prok.sam" ) or die "Damn. $!";
+    open( my $RV_EUK, ">", "$mapDir/rev_euk.sam" )  or die "Damn. $!";
+    open( my $FW_EUK_FQ, ">", "$mapDir/fwd_euk_unmapped.fastq" )
+        or die "Damn. $!";
+    open( my $RV_EUK_FQ, ">", "$mapDir/rev_euk_unmapped.fastq" )
+        or die "Damn. $!";
+    open( FH, "$mapDir/mapped.sam" ) or die "Damn. $!";
+    while (<FH>) {
+        chomp;
+        next if (/^\@/);
+        my $samline = $_;
+        Mapping::parsePairedUnmapped( $samline, $UMAP1, $UMAP2 );
+        Mapping::parsePairedMapped(
+            samline    => $samline,
+            paired_out => $PR_EUK,
+            nonproper  => $NP_EUK
+        );
+        Mapping::parseSingles(
+            samline  => $samline,
+            unMapFwd => $FW_EUK_FQ,
+            unMapRev => $RV_EUK_FQ,
+            MapFwd   => $FW_EUK,
+            MapRev   => $RV_EUK
+        );
+    }
+}
 
 sub printVersion {
     print basename($0), " version: $version\n";
