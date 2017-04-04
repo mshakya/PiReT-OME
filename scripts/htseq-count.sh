c88f1f3b4179d5d9a2bb8269337b0fe1dd9b7076
Migun Shakya <microbeatic@gmail.com>
2017-04-04 09:07:41 -0600

    remove

--- scripts/htseq-count.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-
-##$PBS_O_WORKDIR=$workdir
-##$PBS_O_PATH=$path
-
-#cd $PBS_O_WORKDIR
-echo "perl $scriptDir/htseq-count.pl $workdir $sample $test\n"
-perl $scriptDir/htseq-count.pl $workdir $sample $test
