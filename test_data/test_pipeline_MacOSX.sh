b329488af260d20be50e556d3467df65d3fd79d5
Migun Shakya <microbeatic@gmail.com>
2017-04-05 08:56:45 -0600

    now tests eukarya

--- test_data/test_pipeline_MacOSX.sh
+++ test_data/test_pipeline_MacOSX.sh
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env bash
 
 find fastqs -iname "*test*R2.fastq" -exec echo `pwd`/{} \; > testR2.txt
 find fastqs -iname "*test*R1.fastq" -exec echo `pwd`/{} \; | sed 's/R1\.fastq/R1\.fastq:/g' > testR1.txt
@@ -7,27 +7,23 @@ printf "spleen\nspleen\nliver\nliver\nliver\nspleen\n" > test_gr.txt
 paste -d "\0" testR1.txt testR2.txt > testR1R2.txt
 paste test_id.txt testR1R2.txt test_gr.txt > test_ed.txt
 rm test_id.txt testR1.txt testR2.txt testR1R2.txt test_gr.txt
-echo "ID\tRawreads_files\tgroup" | cat - test_ed.txt > test_experimental_design.txt
+echo -e "ID\tRawreads_files\tgroup" | cat - test_ed.txt > test_experimental_design.txt
 rm test_ed.txt
 
 
 
 printf "running pipeline now......\n"
 
-printf "perl ../runPiReT.pl -test_kingdom both \
+printf "runPiReT-OME -test_kingdom eukarya \
 -significant_pvalue 0.5 -exp test_experimental_design.txt \
--d pipeline_test_both \
--prokaryote_fasta data/test_prok.fa \
+-d pipeline_test_euk \
 -eukarya_fasta data/eukarya_test.fa -index_ref_bt2 test_index \
--gff_eukarya data/eukarya_test.gff3 -gff_prokaryote data/test_prok.gff \
--test_method both -gene_coverage_fasta data/test_prok.fa\n"
+-gff_eukarya data/eukarya_test.gff3"
 
-perl ../runPiReT.pl -test_kingdom both \
+runPiReT-OME -test_kingdom eukarya \
 -significant_pvalue 0.5 -exp test_experimental_design.txt \
--d pipeline_test_both \
--prokaryote_fasta data/test_prok.fa \
+-d pipeline_test_euk \
 -eukarya_fasta data/eukarya_test.fa -index_ref_bt2 test_index \
--gff_eukarya data/eukarya_test.gff3 -gff_prokaryote data/test_prok.gff \
--test_method both -gene_coverage_fasta data/test_prok.fa
+-gff_eukarya data/eukarya_test.gff3
 
 printf "running pipeline finished"
