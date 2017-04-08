36f4e7c5e310adf51d10e5d1616778d78a75d8e2
Migun Shakya <microbeatic@gmail.com>
2017-04-08 17:44:21 -0600

    module for variant calling

---
+++ lib/PiReT/Variant.pm
@@ -0,0 +1,34 @@
+#!/usr/bin/perl
+
+=head1 NAME
+
+PiReT::Variant
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+=head1 SYNOPSIS
+
+This calls variants from mapped reads
+
+Perhaps a little code snippet.
+
+    use PiReT::Variant;
+
+    ...
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+
+package Variant;
+use Exporter;
+@ISA    = ('Exporter');
