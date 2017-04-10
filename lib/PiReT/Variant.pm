bdee97302179a0360a340dd8a9e7babf85f872cb
Migun Shakya <microbeatic@gmail.com>
2017-04-10 17:19:47 -0600

    added other required modules

--- lib/PiReT/Variant.pm
+++ lib/PiReT/Variant.pm
@@ -1,5 +1,11 @@
 #!/usr/bin/perl
 
+use strict;
+use warnings;
+use FindBin qw($Bin);
+use lib "$Bin/../";
+use PiReT::Map;
+
 =head1 NAME
 
 PiReT::Variant
@@ -31,4 +37,24 @@ Perhaps a little code snippet.
 
 package Variant;
 use Exporter;
-@ISA    = ('Exporter');
+
+################################################################################
+sub call_SNPs {
+    my %args             = @_;
+    my $ref = $args{ref};
+    my $aln_file = $args{aln_file};
+    my $bc_file = $args{bc_file};
+    my $vc_output = $args{vc_output};
+
+    my $variant_call = "samtools mpileup -go "
+                        ."$bc_file -f $ref $aln_file ";
+
+    my $bcf_call = "bcftools call -vmO z -o $vc_output $bc_file";
+
+    Map::executeCommand($variant_call);
+    Map::executeCommand($bcf_call)
+}
+################################################################################
+
+################################################################################
+1;    # End of PiReT::Variant
