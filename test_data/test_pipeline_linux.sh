53f9a82401da8d012b09b9ceefe4261600514994
Migun Shakya <microbeatic@gmail.com>
2017-07-17 13:11:46 -0600

    bt2 to ht2

--- test_data/test_pipeline_linux.sh
+++ test_data/test_pipeline_linux.sh
@@ -19,7 +19,7 @@ printf "running pipeline now"
 printf "../bin/runPiReT-OME \
 -exp test_experimental_design.txt \
 -d pipeline_test_euk \
--eukarya_fasta data/eukarya_test.fa -index_ref_bt2 test_index \
+-eukarya_fasta data/eukarya_test.fa -index_ref_ht2 test_index \
 -gff_eukarya data/eukarya_test.gff3"
 
 ../bin/runPiReT-OME \
